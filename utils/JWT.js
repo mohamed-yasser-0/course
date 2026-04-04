c4c948f1182e6bd50f498ef50b5ed9c56ac09c0a
mohamed-yasser-0 <m.yasser.dev@gmail.com>
2026-04-04 09:23:34 +0200

    Update JWT token expiry to 1 day

--- utils/JWT.js
+++ utils/JWT.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = async (payload) => {
-    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1m' });
+    const token = jwt.sign(payload, process.env.JWT_SECRET_KEY, { expiresIn: '1d' });
     return token;
 
 }
